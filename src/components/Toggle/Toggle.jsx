acb95d84dd21db00ec87dd16ebd9ab6897624999
Júlia Acosta <juliagabriela98@gmail.com>
2023-02-16 19:44:48 -0300

    finish confetti logic

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import styles from "./styles.module.css";
+
 export default function Toggle() {
-    return <button>Lista/Mapa</button>;
+    const [toggle, setToggle] = useState(true);
+
+    const onClick = () => {
+        setToggle((prev) => !toggle);
+    };
+
+    return (
+        <div onClick={onClick} className={styles.containerToggle}>
+            <button
+                className={
+                    toggle ? styles.filledButton : styles.notFilledButton
+                }
+            >
+                Lista
+            </button>
+            <button
+                className={
+                    !toggle ? styles.filledButton : styles.notFilledButton
+                }
+            >
+                Mapa
+            </button>
+        </div>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import Header from "../components/Header/Header";
 import RecomendedBlocks from "../components/RecomendedBlocks/RecomendedBlocks";
+import Confetti from "react-confetti";
+import { useState, useRef, useEffect } from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
 
 function Home() {
+    const { width, height } = useWindowSize();
+
     return (
         <body>
             <Header />
+            <Confetti
+                tweenDuration={1000}
+                numberOfPieces={400}
+                width={width}
+                height={height}
+                recycle={false}
+            />
             <RecomendedBlocks />
         </body>
     );
